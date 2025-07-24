cb519dfc6ba96ea96f46497452424820549a605e
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-24 22:11:31 +0300

    Created sample queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import Author, Book, Library, Librarian
 
 # Query to get all books by a specific author
 def get_all_books_by_author(name):
@@ -23,6 +23,7 @@ def get_all_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
-        return f"No library found with name: {library_name}"
+        librarian = Librarian.objects.get(library=library)  # required line
+        return librarian.name
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return f"No librarian found for library: {library_name}"
