e4bf2def6402975dc44a89eafd44e46b6a8465df
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-26 13:12:39 +0300

    Created a function-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def display_all_books(requst):
+    books = Book.objects.all()
+    context = { 'books': books }
+    
