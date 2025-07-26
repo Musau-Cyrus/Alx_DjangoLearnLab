acbf3aa2d8906d42e1bf9214eccb326a3e45e080
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-26 13:31:07 +0300

    Created a class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def display_all_books(request):
     books = Book.objects.all()
     context = { 'books': books }
     return render(request, 'relationship_app/list_books.html', context)
+
+# Class based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library-details.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.book.all()
+        return context
+    
