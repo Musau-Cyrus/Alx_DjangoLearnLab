7a06ddfc06190c923ad3bd2c85d827b41edd3e70
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-27 15:28:01 +0300

    Task 3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class Author(models.Model):
     name = models.CharField(length = 100)
     
@@ -26,3 +31,43 @@ class Librarian(models.Model):
     
     def __str__(self):
         return self.name
+    
+class UserProfile(models.Model):
+        ROLE_CHOICES = [
+            ('Admin', 'Admin'),
+            ('Librarian', 'Librarian'),
+            ('Member', 'Member'),
+        ]
+    
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    
+        def __str__(self):
+            return f"{self.user.username} - {self.role}"
+
+# Automatically create UserProfile when a new User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='Member')  # default to Member
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+   
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.PositiveIntegerField()
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
+    def __str__(self):
+        return self.title
