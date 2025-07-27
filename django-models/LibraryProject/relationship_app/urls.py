4ba646fdc51344611e86151e8e3568ed5676e0d4
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-27 13:25:53 +0300

    User Authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('books/', views.display_all_books, name='list_books'),
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
