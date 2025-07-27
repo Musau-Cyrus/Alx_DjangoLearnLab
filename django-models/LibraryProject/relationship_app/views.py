d8e8b2142bb0e0d6e1f2d400e57040ab59225a34
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-27 13:23:00 +0300

    User Authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from .models import Book
-from .models import Library
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .models import Book, Library
 from django.views.generic.detail import DetailView
+
 
 # Create your views here.
 def display_all_books(request):
@@ -20,3 +23,15 @@ class LibraryDetailView(DetailView):
         context['books'] = self.object.book.all()
         return context
     
+# User Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
