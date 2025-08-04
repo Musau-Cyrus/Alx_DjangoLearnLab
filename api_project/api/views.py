1ec43c20e139ad041bacddb07b3e611d061df092
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-08-04 23:10:40 +0300

    Authentication and Permisions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can view books
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Require authentication for all actions
+    
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list' or self.action == 'retrieve':
+            # Allow authenticated users to read books
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'create' or self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
+            # Only admin users can create, update, or delete books
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        
+        return [permission() for permission in permission_classes]
+
+class CustomAuthToken(ObtainAuthToken):
+    permission_classes = [AllowAny]  # Anyone can request a token
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'username': user.username
+        })
