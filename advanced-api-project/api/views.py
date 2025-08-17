c42669c3684e2b770e4b98dfb9e87eb62f5b2de0
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-08-17 12:17:12 +0300

    Advanced API development

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Book, Author
