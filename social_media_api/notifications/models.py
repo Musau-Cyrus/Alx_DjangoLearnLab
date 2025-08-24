654c15933f26621bd89c86589b4003e8f5cf3d2f
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-08-24 22:30:16 +0300

    Implemented notification and like functionality

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -14,7 +14,7 @@ class Notification(models.Model):
     target_object_id=models.PositiveIntegerField()
     target = GenericForeignKey("target_content_type", "target_object_id")
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     read=models.BooleanField(default=False)
 
     def __str__(self):
